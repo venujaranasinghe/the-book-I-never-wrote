c6c77557ed5546c45f3f06679fc8838d56bb4be4
venujaranasinghe <it23225510@my.sliit.lk>
2025-09-08 12:04:32 +0530

    Add .gitignore for .NET and frontend files
    Introduced a comprehensive .gitignore to exclude build artifacts, IDE files, OS files, and sensitive configuration for .NET, Node.js, and common editors. Also includes updated build outputs and cache files from thebook.api due to a new build.

--- thebook.api/obj/Debug/net9.0/thebook.api.AssemblyInfo.cs
+++ thebook.api/obj/Debug/net9.0/thebook.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thebook.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870c9f97a758ad9b8efc5f97706169ba0217f1ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4676124d22ced3adba3a88e18ed45347641b3ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("thebook.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thebook.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
